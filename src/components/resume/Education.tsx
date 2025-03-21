6c8c0f92dcef900f75f3c383b99223d2244ea9ff
kavindu0818 <kavindumaduranga184@gamil.com>
2025-03-21 14:40:28 +0530

    Add Education

--- src/components/resume/Education.tsx
+++ src/components/resume/Education.tsx
@@ -14,7 +14,13 @@ const Education = () => {
             {/* You can add more content here */}
           </div>
           <div className="flex flex-col gap-12 relative w-full sm:w-auto">
-            {/* Content here */}
+
+            <ResumeCard
+                title=" BSc (Hons) Computer Science"
+                subTitle="University of Bolton (2025-2026)"
+                result=""
+                des="The BSc (Hons) Computer Science at the University of Bolton (2025-2026) covers software development, AI, cybersecurity, and cloud computing. It emphasizes hands-on learning, problem-solving, and industry-relevant skills, preparing graduates for careers in tech."
+            />
             <ResumeCard
                 title="Graduate Diploma in Software Engineering"
                 subTitle="Institute of Software Engineering - IJSE (2023 - 2025)"
@@ -27,12 +33,12 @@ const Education = () => {
                 result=""
                 des="In 2021, I completed my Advanced Level exam in the Physics stream, studying Combined Mathematics, Physics, and Chemistry. These subjects enhanced my problem-solving, analytical, and scientific skills, providing a strong foundation in calculus, mechanics, electromagnetism, and chemistry for further studies in engineering, technology, or scientific research."
             />
-            <ResumeCard
-                title="G.C.E Ordinary Level"
-                subTitle="2017"
-                result=""
-                des="I completed my G.C.E. Ordinary Level examination in 2017 at Dambulla Central College, successfully facing nine subjects. This education provided me with a strong academic foundation, enhancing my knowledge in key subjects while developing critical thinking, problem-solving, and analytical skills essential for higher studies and future career growth."
-            />
+            {/*<ResumeCard*/}
+            {/*    title="G.C.E Ordinary Level"*/}
+            {/*    subTitle="2017"*/}
+            {/*    result=""*/}
+            {/*    des="I completed my G.C.E. Ordinary Level examination in 2017 at Dambulla Central College, successfully facing nine subjects. This education provided me with a strong academic foundation, enhancing my knowledge in key subjects while developing critical thinking, problem-solving, and analytical skills essential for higher studies and future career growth."*/}
+            {/*/>*/}
           </div>
         </div>
       </motion.div>
